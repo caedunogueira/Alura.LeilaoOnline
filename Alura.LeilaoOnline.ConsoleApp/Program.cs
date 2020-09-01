047f1637b27a47c28bcec1fb23a61f01d7170e74
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-09-01 07:26:59 -0300

    Exercícios com XUnit

---
+++ Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            LeilaoComVariosLances();
+            LeilaoComApenasUmLance();
+        }
+
+        private static void LeilaoComVariosLances()
+        {
+            // Arrange - cenário
+            var modalidade = new MaiorValor();
+            var leilao = new Leilao("Van Gogh", modalidade);
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1200;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void LeilaoComApenasUmLance()
+        {
+            // Arrange - cenário
+            var modalidade = new MaiorValor();
+            var leilao = new Leilao("Van Gogh", modalidade);
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+            Verifica(valorEsperado, valorObtido);
+        }
+
+        private static void Verifica(double esperado, double obtido)
+        {
+            var cor = Console.ForegroundColor;
+            if (esperado == obtido)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"TESTE FALHOU! Esperado: {esperado}, obtido: {obtido}.");
+            }
+            Console.ForegroundColor = cor;
+        }
+    }
+}
